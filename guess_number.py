af4f99f8bfc2ee24d5b3419112b4f5f2754d364f
harryyang1982 <harry.seunghoon.yang@gmail.com>
2022-03-20 00:10:01 +0900

    error fixed

--- guess_number.py
+++ guess_number.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import random
 
 guess_number = random.randint(1, 100)
@@ -10,6 +8,6 @@ while (user_input is not guess_number):
         print("숫자가 너무 큽니다.")
     else:
         print("숫자가 너무 작습니다.")
-    user_input = input("다시 입력하세요. \n")
+    user_input = int(input("다시 입력하세요. \n"))
 else:
     print("정답입니다. 입력한 숫자는", user_input, "입니다!")
