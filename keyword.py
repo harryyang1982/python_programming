a74e2738abb7eaa5ea5205a66386a5c73910cd1b
harryyang1982 <harry.seunghoon.yang@gmail.com>
2022-03-21 21:47:35 +0900

    chapter5 cleared

---
+++ keyword.py
@@ -0,0 +1,5 @@
+def print_something(my_name, your_name):
+    print("Hello {0}, My name is {1}".format(your_name, my_name))
+    
+print_something("Seunghoon", "Yeojeong")
+print_something(your_name = "Yeojeong", my_name = "Seunghoon")
