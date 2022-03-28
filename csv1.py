bab066ba06c20f7596c6ddaf024882399b734b9f
harryyang1982 <harry.seunghoon.yang@gmail.com>
2022-03-28 14:40:35 +0900

    chapter13 cleared

---
+++ csv1.py
@@ -0,0 +1,20 @@
+line_counter = 0
+data_header = []
+customer_list = []
+
+with open("source/Ch13/customers.csv") as customer_data:
+    while 1:
+        data = customer_data.readline()
+        if not data:break
+        if line_counter == 0:
+            data_header = data.split(",")
+            
+        else:
+            customer_list.append(data.split(","))
+            
+        line_counter += 1
+        
+print("Header:", data_header)
+for i in range(0, 10):
+    print("Data", i, ":", customer_list[i])
+print(len(customer_list))
